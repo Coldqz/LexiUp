626e0becbbd0ac2bf44a12a420f770d4a26f7829
Coldqz <blizzcold1@gmail.com>
2025-09-12 19:08:53 +0200

    hilt dependency was added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -61,5 +62,8 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
+
+    implementation(libs.dagger.hilt.android)
+    ksp(libs.hilt.android.compiler)
 
 }
