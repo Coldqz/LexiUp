c55191c66cea92d168e4b7dd2b196adaf97dd8c8
Coldqz <blizzcold1@gmail.com>
2025-11-01 21:29:29 +0100

    changed icon names, and added new icons

--- app/src/main/java/com/coldzz/lexiup/features/blocks/domain/LearningLevelIndicator.kt
+++ app/src/main/java/com/coldzz/lexiup/features/blocks/domain/LearningLevelIndicator.kt
@@ -4,8 +4,8 @@ import androidx.annotation.DrawableRes
 import com.coldzz.lexiup.R
 
 enum class LearningLevelIndicator(@param:DrawableRes val resourceId: Int) {
-    One(R.drawable.learning_level_1),
-    Two(R.drawable.learning_level_2),
-    Three(R.drawable.learning_level_3),
-    Four(R.drawable.learning_level_4)
+    One(R.drawable.ic_learning_level_1),
+    Two(R.drawable.ic_learning_level_2),
+    Three(R.drawable.ic_learning_level_3),
+    Four(R.drawable.ic_learning_level_4)
 }
